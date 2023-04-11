709f94709d22215e310ab50c49b2c9a73b54c455
Francesca_Dattoma <francdatt@gmail.com>
2023-04-11 19:11:12 +0200

    slider aggiunto

---
+++ main.js
@@ -0,0 +1,15 @@
+/*scroll navbar*/
+
+let mainNavbar = document.querySelector('#mainNavbar');
+
+window.addEventListener('scroll', ()=>{
+
+
+    mainNavbar.classList.add('background-lastC');
+    mainNavbar.classList.remove('bg-transparent');
+
+
+
+
+
+})

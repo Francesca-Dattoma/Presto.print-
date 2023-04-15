ff49aa8d3aa8cb648ed8a779f71260b0f84814c2
Francesca_Dattoma <francdatt@gmail.com>
2023-04-15 21:41:10 +0200

    aggiunte

--- main.js
+++ main.js
@@ -1,15 +1,127 @@
+
+
 /*scroll navbar*/
 
 let mainNavbar = document.querySelector('#mainNavbar');
 
 window.addEventListener('scroll', ()=>{
 
+         
+    if(window.scrollY > 0){
 
-    mainNavbar.classList.add('background-lastC');
-    mainNavbar.classList.remove('bg-transparent');
+        mainNavbar.classList.remove('bg-transparent');
+        mainNavbar.classList.add('background-accentC');
+
+        
+        // mainNavbar.style.padding = '20px 0px';
+
+    } else {
+
+        mainNavbar.classList.remove('background-accentC');
+        mainNavbar.classList.add('bg-transparent');
+
+        
+        // mainNavbar.style.padding = '10px 0px';
+
+            
+
+    }
 
 
+})
 
+
+/*counter*/
+
+let firstSpan = document.querySelector('#first-span');
+let secondSpan = document.querySelector('#second-span');
+let thirdSpan = document.querySelector('#third-span');
+
+function createInterval(finalNumber, element){
+
+    //intervallo inizia a contare da zero
+    let counter = 0;
+
+    // per avviare il contatore: setInterval
+
+    let interval = setInterval( ()=>{
+
+        //finisci di contare fino al finalNumber
+
+        if(counter < finalNumber){
+
+            //incremento del contatore
+            counter++
+            element.innerHTML = counter;
+            
+
+        } else {
+
+            //per stoppare il loop del counter
+
+            clearInterval(interval);
+
+        }
+            // tempo del contatore: 1 millisecondo
+    }, 1)
+
+}
+
+createInterval();
+
+
+createInterval(800, firstSpan);
+createInterval(1000, secondSpan);
+createInterval(1200, thirdSpan);
+
+
+//evento mousenter su icone delle cards
+//SelectorAll genera una nodelist con array-like
+
+let icone = document.querySelectorAll('#icone');
+
+let cards =  document.querySelectorAll('.card-custom');
+
+let cardsConfirm = false;
+
+console.log (cards);
+
+cards.forEach((carta, i)=>{
+
+        carta.addEventListener('mouseenter', ()=>{
+
+            if(cardsConfirm == false){
+
+            icone[i].classList.remove('text-primaryC');
+            icone[i].classList.add('text-secondaryC');
+        }else{
+
+            icone[i].classList.remove('text-lastC');
+
+        }
+
+     })
+
+        carta.addEventListener('mouseleave', ()=>{
+
+            if(cardsConfirm == false){
+
+                icone[i].classList.remove('text-secondaryC');
+                icone[i].classList.add('text-lastC');
+
+                cardsConfirm = true;
+
+
+            } else {
+
+                icone[i].classList.add('text-primaryC');
+                
+                cardsConfirm = true;
+
+            }
+
+         
+        })
 
 
 })
